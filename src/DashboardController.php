f5f119d7d369af4f37d16a0f15145273aa7027f9
Facundo Lezcano <facu.lezca@gmail.com>
2021-08-16 11:28:08 -0300

    Initial commit

---
+++ src/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Iutrace\Dashboard;
+
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class DashboardController extends Controller
+{
+    public function data(Request $request, DashboardServiceProvider $dashboardProvider){
+        $data = $request->validate([
+            'metric' => [
+                'required',
+                Rule::in($dashboardProvider->metrics)
+            ],
+        ]);
+    }
+}
---
+++ src/DashboardServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Iutrace\Dashboard;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SplFileInfo;
+
+class DashboardServiceProvider extends ServiceProvider
+{
+    public $metricsPath;
+
+    public $metrics = [];
+
+    public function boot(){
+        $this->registerRoutes();
+
+        $this->metricsPath = App::basePath('Metrics');
+
+        /* @var $file SplFileInfo */
+        foreach (new RecursiveIteratorIterator (new RecursiveDirectoryIterator ($this->metricsPath)) as $file)
+        {
+            $this->metrics[] = $file->getPathname();
+        }
+    }
+
+    protected function registerRoutes(){
+        Route::get('dashboard/data', [DashboardController::class, 'data']);
+    }
+}
